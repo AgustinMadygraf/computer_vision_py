a2f9707e3d029ae428b16a3a0749aefa887ee995
AgustinMadygraf <agustin.mtto.madygraf>
2025-09-04 17:16:06 -0300

    feat: Refactor camera stream creation to use direct imports for gateways

--- src/composition_root.py
+++ src/composition_root.py
@@ -11,6 +11,8 @@ from src.infrastructure.fastapi.static_server import app as fastapi_app
 from src.infrastructure.fastapi import stream_adapter
 from src.entities.camera_stream import CameraStreamInterface
 from src.infrastructure.fastapi.stream_adapter import FastAPICameraHTTPAdapter
+from src.interface_adapters.gateway.camera_discovery_gateway import CameraDiscoveryGateway
+from src.interface_adapters.gateway.wifi_credentials_gateway import WifiCredentialsGateway
 
 logger = get_logger("CompositionRoot")
 
@@ -18,8 +20,8 @@ def create_camera_stream() -> CameraStreamInterface:
     "Crea un stream de cámara según la configuración dinámica de hardware, inyectando dependencias explícitamente."
     try:
         logger.info("Instanciando gateways y servicios para DiscoverCamerasUseCase")
-        usb_gateway = __import__('src.interface_adapters.gateway.camera_discovery_gateway', fromlist=['CameraDiscoveryGateway']).CameraDiscoveryGateway()
-        wifi_gateway = __import__('src.interface_adapters.gateway.wifi_credentials_gateway', fromlist=['WifiCredentialsGateway']).WifiCredentialsGateway()
+        usb_gateway = CameraDiscoveryGateway()
+        wifi_gateway = WifiCredentialsGateway()
         cameras_usecase = DiscoverCamerasUseCase(usb_gateway, wifi_gateway)
         cameras = cameras_usecase.execute()
         logger.info("Cámaras detectadas: %s", cameras)
